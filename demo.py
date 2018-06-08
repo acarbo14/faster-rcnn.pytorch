073e8bf0b81f2d9799740ffddd3b7baee71dee7d
Adria Carbo <acarbo@v5.imatge.upc.edu>
2018-06-08 14:23:45 +0200

    demo.py

--- demo.py
+++ demo.py
@@ -61,7 +61,7 @@ def parse_args():
                       nargs=argparse.REMAINDER)
   parser.add_argument('--load_dir', dest='load_dir',
                       help='directory to load models',
-                      default="/srv/share/jyang375/models")
+                      default="/work/acarbo/faster_rcnn/data/in_training_models")
   parser.add_argument('--image_dir', dest='image_dir',
                       help='directory to load images for demo',
                       default="images")
@@ -164,8 +164,10 @@ if __name__ == '__main__':
   input_dir = args.load_dir + "/" + args.net + "/" + args.dataset
   if not os.path.exists(input_dir):
     raise Exception('There is no input directory for loading network from ' + input_dir)
-  load_name = os.path.join(input_dir,
-    'faster_rcnn_{}_{}_{}.pth'.format(args.checksession, args.checkepoch, args.checkpoint))
+  
+  load_name = [os.path.join(input_dir,
+            'faster_rcnn_{}_{}_{}.pth'.format(args.checksession, i, args.checkpoint)) for i in range(2,6)]
+  print(load_name)
 
   pascal_classes = np.asarray(['__background__',
                        'aeroplane', 'bicycle', 'bird', 'boat',
@@ -188,197 +190,198 @@ if __name__ == '__main__':
     pdb.set_trace()
 
   fasterRCNN.create_architecture()
-
-  print("load checkpoint %s" % (load_name))
-  if args.cuda > 0:
-    checkpoint = torch.load(load_name)
-    print("Per cuda eh")
-  else:
-    checkpoint = torch.load(load_name, map_location=(lambda storage, loc: storage))
-    print("Sin cuda eh")
-  print(type(checkpoint))
-  print(checkpoint.keys())
-  fasterRCNN.load_state_dict(checkpoint['model'])
-  if 'pooling_mode' in checkpoint.keys():
-    cfg.POOLING_MODE = checkpoint['pooling_mode']
-
-
-  print('load model successfully!')
-
-  # pdb.set_trace()
-
-  print("load checkpoint %s" % (load_name))
-
-  # initilize the tensor holder here.
-  im_data = torch.FloatTensor(1)
-  im_info = torch.FloatTensor(1)
-  num_boxes = torch.LongTensor(1)
-  gt_boxes = torch.FloatTensor(1)
-
-  # ship to cuda
-  if args.cuda > 0:
-    im_data = im_data.cuda()
-    im_info = im_info.cuda()
-    num_boxes = num_boxes.cuda()
-    gt_boxes = gt_boxes.cuda()
-
-  # make variable
-  im_data = Variable(im_data, volatile=True)
-  im_info = Variable(im_info, volatile=True)
-  num_boxes = Variable(num_boxes, volatile=True)
-  gt_boxes = Variable(gt_boxes, volatile=True)
-
-  if args.cuda > 0:
-    cfg.CUDA = True
-
-  if args.cuda > 0:
-    fasterRCNN.cuda()
-
-  fasterRCNN.eval()
-
-  start = time.time()
-  max_per_image = 100
-  thresh = 0.05
-  vis = True
-
-  webcam_num = args.webcam_num
-  # Set up webcam or get image directories
-  if webcam_num >= 0 :
-    cap = cv2.VideoCapture(webcam_num)
-    num_images = 0
-  else:
-    imglist = os.listdir(args.image_dir)
-    num_images = len(imglist)
-
-  print('Loaded Photo: {} images.'.format(num_images))
+  for i in range(len(load_name)):
+    print("load checkpoint %s" % (load_name[i]))
+    if args.cuda > 0:
+      checkpoint = torch.load(load_name[i])
+      print("Per cuda eh")
+    else:
+      checkpoint = torch.load(load_name[i], map_location=(lambda storage, loc: storage))
+      print("Sin cuda eh")
+    print(type(checkpoint))
+    print(checkpoint.keys())
+    fasterRCNN.load_state_dict(checkpoint['model'])
+    if 'pooling_mode' in checkpoint.keys():
+      cfg.POOLING_MODE = checkpoint['pooling_mode']
 
 
-  while (num_images >= 0):
-      total_tic = time.time()
-      if webcam_num == -1:
-        num_images -= 1
+    print('load model successfully!')
 
-      # Get image from the webcam
-      if webcam_num >= 0:
-        if not cap.isOpened():
-          raise RuntimeError("Webcam could not open. Please check connection.")
-        ret, frame = cap.read()
-        im_in = np.array(frame)
-      # Load the demo image
-      else:
-        im_file = os.path.join(args.image_dir, imglist[num_images])
-        # im = cv2.imread(im_file)
-        im_in = np.array(imread(im_file))
-      if len(im_in.shape) == 2:
-        im_in = im_in[:,:,np.newaxis]
-        im_in = np.concatenate((im_in,im_in,im_in), axis=2)
-      # rgb -> bgr
-      im = im_in[:,:,::-1]
+    # pdb.set_trace()
 
-      blobs, im_scales = _get_image_blob(im)
-      assert len(im_scales) == 1, "Only single-image batch implemented"
-      im_blob = blobs
-      im_info_np = np.array([[im_blob.shape[1], im_blob.shape[2], im_scales[0]]], dtype=np.float32)
+    print("load checkpoint %s" % (load_name))
 
-      im_data_pt = torch.from_numpy(im_blob)
-      im_data_pt = im_data_pt.permute(0, 3, 1, 2)
-      im_info_pt = torch.from_numpy(im_info_np)
+    # initilize the tensor holder here.
+    im_data = torch.FloatTensor(1)
+    im_info = torch.FloatTensor(1)
+    num_boxes = torch.LongTensor(1)
+    gt_boxes = torch.FloatTensor(1)
 
-      im_data.data.resize_(im_data_pt.size()).copy_(im_data_pt)
-      im_info.data.resize_(im_info_pt.size()).copy_(im_info_pt)
-      gt_boxes.data.resize_(1, 1, 5).zero_()
-      num_boxes.data.resize_(1).zero_()
+    # ship to cuda
+    if args.cuda > 0:
+      im_data = im_data.cuda()
+      im_info = im_info.cuda()
+      num_boxes = num_boxes.cuda()
+      gt_boxes = gt_boxes.cuda()
 
-      # pdb.set_trace()
-      det_tic = time.time()
+    # make variable
+    im_data = Variable(im_data, volatile=True)
+    im_info = Variable(im_info, volatile=True)
+    num_boxes = Variable(num_boxes, volatile=True)
+    gt_boxes = Variable(gt_boxes, volatile=True)
 
-      rois, cls_prob, bbox_pred, \
-      rpn_loss_cls, rpn_loss_box, \
-      RCNN_loss_cls, RCNN_loss_bbox, \
-      rois_label = fasterRCNN(im_data, im_info, gt_boxes, num_boxes)
+    if args.cuda > 0:
+      cfg.CUDA = True
 
-      scores = cls_prob.data
-      boxes = rois.data[:, :, 1:5]
+    if args.cuda > 0:
+      fasterRCNN.cuda()
 
-      if cfg.TEST.BBOX_REG:
-          # Apply bounding-box regression deltas
-          box_deltas = bbox_pred.data
-          if cfg.TRAIN.BBOX_NORMALIZE_TARGETS_PRECOMPUTED:
-          # Optionally normalize targets by a precomputed mean and stdev
-            if args.class_agnostic:
-                if args.cuda > 0:
-                    box_deltas = box_deltas.view(-1, 4) * torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_STDS).cuda() \
-                               + torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_MEANS).cuda()
-                else:
-                    box_deltas = box_deltas.view(-1, 4) * torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_STDS) \
-                               + torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_MEANS)
+    fasterRCNN.eval()
 
-                box_deltas = box_deltas.view(1, -1, 4)
-            else:
-                if args.cuda > 0:
-                    box_deltas = box_deltas.view(-1, 4) * torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_STDS).cuda() \
-                               + torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_MEANS).cuda()
-                else:
-                    box_deltas = box_deltas.view(-1, 4) * torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_STDS) \
-                               + torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_MEANS)
-                box_deltas = box_deltas.view(1, -1, 4 * len(pascal_classes))
+    start = time.time()
+    max_per_image = 100
+    thresh = 0.05
+    vis = True
 
-          pred_boxes = bbox_transform_inv(boxes, box_deltas, 1)
-          pred_boxes = clip_boxes(pred_boxes, im_info.data, 1)
-      else:
-          # Simply repeat the boxes, once for each class
-          pred_boxes = np.tile(boxes, (1, scores.shape[1]))
+    webcam_num = args.webcam_num
+    # Set up webcam or get image directories
+    if webcam_num >= 0 :
+      cap = cv2.VideoCapture(webcam_num)
+      num_images = 0
+    else:
+      imglist = os.listdir(args.image_dir)
+      num_images = len(imglist)
 
-      pred_boxes /= im_scales[0]
+    print('Loaded Photo: {} images.'.format(num_images))
 
-      scores = scores.squeeze()
-      pred_boxes = pred_boxes.squeeze()
-      det_toc = time.time()
-      detect_time = det_toc - det_tic
-      misc_tic = time.time()
-      if vis:
-          im2show = np.copy(im)
-      for j in xrange(1, len(pascal_classes)):
-          inds = torch.nonzero(scores[:,j]>thresh).view(-1)
-          # if there is det
-          if inds.numel() > 0:
-            cls_scores = scores[:,j][inds]
-            _, order = torch.sort(cls_scores, 0, True)
-            if args.class_agnostic:
-              cls_boxes = pred_boxes[inds, :]
-            else:
-              cls_boxes = pred_boxes[inds][:, j * 4:(j + 1) * 4]
-            
-            cls_dets = torch.cat((cls_boxes, cls_scores.unsqueeze(1)), 1)
-            # cls_dets = torch.cat((cls_boxes, cls_scores), 1)
-            cls_dets = cls_dets[order]
-            keep = nms(cls_dets, cfg.TEST.NMS, force_cpu=not cfg.USE_GPU_NMS)
-            cls_dets = cls_dets[keep.view(-1).long()]
-            if vis:
-              im2show = vis_detections(im2show, pascal_classes[j], cls_dets.cpu().numpy(), 0.5)
 
-      misc_toc = time.time()
-      nms_time = misc_toc - misc_tic
+    while (num_images >= 0):
+        total_tic = time.time()
+        if webcam_num == -1:
+          num_images -= 1
 
-      if webcam_num == -1:
-          sys.stdout.write('im_detect: {:d}/{:d} {:.3f}s {:.3f}s   \r' \
-                           .format(num_images + 1, len(imglist), detect_time, nms_time))
-          sys.stdout.flush()
+        # Get image from the webcam
+        if webcam_num >= 0:
+          if not cap.isOpened():
+            raise RuntimeError("Webcam could not open. Please check connection.")
+          ret, frame = cap.read()
+          im_in = np.array(frame)
+        # Load the demo image
+        else:
+          im_file = os.path.join(args.image_dir, imglist[num_images])
+          # im = cv2.imread(im_file)
+          im_in = np.array(imread(im_file))
+        if len(im_in.shape) == 2:
+          im_in = im_in[:,:,np.newaxis]
+          im_in = np.concatenate((im_in,im_in,im_in), axis=2)
+        # rgb -> bgr
+        im = im_in[:,:,::-1]
 
-      if vis and webcam_num == -1:
-          # cv2.imshow('test', im2show)
-          # cv2.waitKey(0)
-          result_path = os.path.join(args.image_dir, imglist[num_images][:-4] + "_det.jpg")
-          cv2.imwrite(result_path, im2show)
-      else:
-          im2showRGB = cv2.cvtColor(im2show, cv2.COLOR_BGR2RGB)
-          cv2.imshow("frame", im2showRGB)
-          total_toc = time.time()
-          total_time = total_toc - total_tic
-          frame_rate = 1 / total_time
-          print('Frame rate:', frame_rate)
-          if cv2.waitKey(1) & 0xFF == ord('q'):
-              break
-  if webcam_num >= 0:
-      cap.release()
-      cv2.destroyAllWindows()
+        blobs, im_scales = _get_image_blob(im)
+        assert len(im_scales) == 1, "Only single-image batch implemented"
+        im_blob = blobs
+        im_info_np = np.array([[im_blob.shape[1], im_blob.shape[2], im_scales[0]]], dtype=np.float32)
+
+        im_data_pt = torch.from_numpy(im_blob)
+        im_data_pt = im_data_pt.permute(0, 3, 1, 2)
+        im_info_pt = torch.from_numpy(im_info_np)
+
+        im_data.data.resize_(im_data_pt.size()).copy_(im_data_pt)
+        im_info.data.resize_(im_info_pt.size()).copy_(im_info_pt)
+        gt_boxes.data.resize_(1, 1, 5).zero_()
+        num_boxes.data.resize_(1).zero_()
+
+        # pdb.set_trace()
+        det_tic = time.time()
+
+        rois, cls_prob, bbox_pred, \
+        rpn_loss_cls, rpn_loss_box, \
+        RCNN_loss_cls, RCNN_loss_bbox, \
+        rois_label = fasterRCNN(im_data, im_info, gt_boxes, num_boxes)
+
+        scores = cls_prob.data
+        boxes = rois.data[:, :, 1:5]
+
+        if cfg.TEST.BBOX_REG:
+            # Apply bounding-box regression deltas
+            box_deltas = bbox_pred.data
+            if cfg.TRAIN.BBOX_NORMALIZE_TARGETS_PRECOMPUTED:
+            # Optionally normalize targets by a precomputed mean and stdev
+              if args.class_agnostic:
+                  if args.cuda > 0:
+                      box_deltas = box_deltas.view(-1, 4) * torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_STDS).cuda() \
+                                 + torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_MEANS).cuda()
+                  else:
+                      box_deltas = box_deltas.view(-1, 4) * torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_STDS) \
+                                 + torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_MEANS)
+
+                  box_deltas = box_deltas.view(1, -1, 4)
+              else:
+                  if args.cuda > 0:
+                      box_deltas = box_deltas.view(-1, 4) * torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_STDS).cuda() \
+                                 + torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_MEANS).cuda()
+                  else:
+                      box_deltas = box_deltas.view(-1, 4) * torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_STDS) \
+                                 + torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_MEANS)
+                  box_deltas = box_deltas.view(1, -1, 4 * len(pascal_classes))
+
+            pred_boxes = bbox_transform_inv(boxes, box_deltas, 1)
+            pred_boxes = clip_boxes(pred_boxes, im_info.data, 1)
+        else:
+            # Simply repeat the boxes, once for each class
+            pred_boxes = np.tile(boxes, (1, scores.shape[1]))
+
+        pred_boxes /= im_scales[0]
+
+        scores = scores.squeeze()
+        pred_boxes = pred_boxes.squeeze()
+        det_toc = time.time()
+        detect_time = det_toc - det_tic
+        misc_tic = time.time()
+        if vis:
+            im2show = np.copy(im)
+        for j in xrange(1, len(pascal_classes)):
+            inds = torch.nonzero(scores[:,j]>thresh).view(-1)
+            # if there is det
+            if inds.numel() > 0:
+              cls_scores = scores[:,j][inds]
+              _, order = torch.sort(cls_scores, 0, True)
+              if args.class_agnostic:
+                cls_boxes = pred_boxes[inds, :]
+              else:
+                cls_boxes = pred_boxes[inds][:, j * 4:(j + 1) * 4]
+              
+              cls_dets = torch.cat((cls_boxes, cls_scores.unsqueeze(1)), 1)
+              # cls_dets = torch.cat((cls_boxes, cls_scores), 1)
+              cls_dets = cls_dets[order]
+              keep = nms(cls_dets, cfg.TEST.NMS, force_cpu=not cfg.USE_GPU_NMS)
+              cls_dets = cls_dets[keep.view(-1).long()]
+              if vis:
+                im2show = vis_detections(im2show, pascal_classes[j], cls_dets.cpu().numpy(), 0.5)
+
+        misc_toc = time.time()
+        nms_time = misc_toc - misc_tic
+
+        if webcam_num == -1:
+            sys.stdout.write('im_detect: {:d}/{:d} {:.3f}s {:.3f}s   \r' \
+                             .format(num_images + 1, len(imglist), detect_time, nms_time))
+            sys.stdout.flush()
+
+        if vis and webcam_num == -1:
+            # cv2.imshow('test', im2show)
+            # cv2.waitKey(0)
+
+            result_path = os.path.join(args.image_dir,detections, imglist[num_images][:-4] + '_det_'+str(i)+'.jpg')
+            cv2.imwrite(result_path, im2show)
+        else:
+            im2showRGB = cv2.cvtColor(im2show, cv2.COLOR_BGR2RGB)
+            cv2.imshow("frame", im2showRGB)
+            total_toc = time.time()
+            total_time = total_toc - total_tic
+            frame_rate = 1 / total_time
+            print('Frame rate:', frame_rate)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    if webcam_num >= 0:
+        cap.release()
+        cv2.destroyAllWindows()
